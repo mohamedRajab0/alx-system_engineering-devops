d6aea872a41de636275f8d5c5f6b80d4a8d28a7c
mohamedRajab0 <elzaimohamed@gmail.com>
2024-06-18 13:58:08 +0300

    frist commit mandatory tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script for handle api"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """unction that queries the Reddit API and returns 
+    the number of subscribers
+    for a given subreddit. If an invalid subreddit is given, the function
+    should return 0."""
+
+    base_url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    request = requests.get(base_url, headers = {'User-agent': 'app/1.0'})
+    if request.status_code == 200:
+        data = request.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
